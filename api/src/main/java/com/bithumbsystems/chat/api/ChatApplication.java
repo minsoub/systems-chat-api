c08d422cb27f803b1963347334be0edea47155b8
rihongo-at-807380035085 <3zaHkd3zJ87T1icEEOSTNLAHlAdGniO9sZT2LJlmeo4=>
2022-06-24 00:23:06 +0900

    chat exception 처리

--- api/src/main/java/com/bithumbsystems/chat/api/ChatApplication.java
+++ api/src/main/java/com/bithumbsystems/chat/api/ChatApplication.java
@@ -2,6 +2,10 @@ package com.bithumbsystems.chat.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
@@ -9,7 +13,12 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 
 
 
-@SpringBootApplication(scanBasePackages = "com.bithumbsystems")
+@SpringBootApplication(scanBasePackages = "com.bithumbsystems", exclude = {
+    MongoAutoConfiguration.class,
+    MongoReactiveAutoConfiguration.class,
+    MongoDataAutoConfiguration.class,
+    EmbeddedMongoAutoConfiguration.class
+})
 @EnableWebFlux
 @ConfigurationPropertiesScan
 @EnableReactiveMongoRepositories("com.bithumbsystems.persistence.mongodb")
@@ -19,15 +28,4 @@ public class ChatApplication {
   public static void main(String[] args) {
     SpringApplication.run(ChatApplication.class, args);
   }
-
-
-//  @Profile("dev|prod|eks-dev")
-//  @EnableAutoConfiguration(
-//      exclude = {
-//          MongoAutoConfiguration.class,
-//          MongoReactiveAutoConfiguration.class,
-//          MongoDataAutoConfiguration.class,
-//          EmbeddedMongoAutoConfiguration.class
-//      })
-//  static class WithoutAutoConfigurationMongo{}
 }
