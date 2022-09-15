48b32f5c3a749dcb4c8dc2e6f52a9f584cffd89b
rihongo-at-807380035085 <3zaHkd3zJ87T1icEEOSTNLAHlAdGniO9sZT2LJlmeo4=>
2022-09-15 11:52:57 +0900

    By default, JVM allows 64MB for direct buffer memory, you can increase it by using JVM option -XX:MaxDirectMemorySize=512m.
    Read more: https://www.java67.com/2014/01/how-to-fix-javalangoufofmemoryerror-direct-byte-buffer-java.html#ixzz7evBZzrXQ

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -14,4 +14,4 @@ export AWS_SECRET_ACCESS_KEY=$3
 echo $AWS_ACCESS_KEY_ID
 echo $AWS_SECRET_ACCESS_KEY
 
-java -Dspring.profiles.active=$1 -jar ./systems-chat-api.jar -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/app -Dio.netty.leakDetectionLevel=advanced
+java -Dspring.profiles.active=$1 -jar ./systems-chat-api.jar -XX:MaxDirectMemorySize=512m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/app -Dio.netty.leakDetectionLevel=advanced
